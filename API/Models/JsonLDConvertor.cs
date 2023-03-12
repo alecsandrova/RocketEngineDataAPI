b4668406070f6e08de5cb56b0d78172260694c22
Andrei Visoiu <visoiuandre@gmail.com>
2023-03-12 22:02:33 +0200

    Parser done

---
+++ API/Models/JsonLDConvertor.cs
@@ -0,0 +1,54 @@
+﻿using MongoDB.Bson.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using ThirdParty.BouncyCastle.Utilities.IO.Pem;
+
+namespace API.Models
+{
+    public class JsonLDConvertor
+    {
+        public String MakeStringOutput(RocketModel rocket)
+        {
+            var context = new JObject { { "@schema", "MongoDB" } };
+            var listaProprietati = JObject.Parse(Newtonsoft.Json.JsonConvert.SerializeObject(rocket, Formatting.Indented));
+
+            listaProprietati.Property("Id").Replace(new JProperty("@id", listaProprietati["Id"]));
+
+            var obiect = new JObject
+            {
+                { "@context", context },
+                { "@type",  "RocketScienceData"},
+                { "@list",  listaProprietati}
+            };
+
+            return Newtonsoft.Json.JsonConvert.SerializeObject(obiect, Formatting.Indented);
+        }
+
+        public String MakeStringOutput(List<RocketModel> rocket)
+        {
+            var context = new JObject { { "@schema", "MongoDB" } };
+            var listaProprietati = JArray.Parse(Newtonsoft.Json.JsonConvert.SerializeObject(rocket, Formatting.Indented));
+
+            foreach (JObject item in listaProprietati.Children<JObject>())
+            {
+                JProperty originalProperty = item.Property("Id");
+                if (originalProperty != null)
+                {
+                    string value = originalProperty.Value.ToString();
+                    item.Remove("Id");
+                    item.First.AddBeforeSelf(new JProperty("@Id", value));
+                }
+            }  
+
+            var obiect = new JObject
+            {
+                { "@context", context },
+                { "@type",  "RocketScienceData"},
+                { "@list",  listaProprietati}
+            };
+
+            return Newtonsoft.Json.JsonConvert.SerializeObject(obiect, Formatting.Indented);
+            
+        }
+    }
+}
--- API/Processors/RocketProcessor.cs
+++ API/Processors/RocketProcessor.cs
@@ -65,18 +65,15 @@ namespace API.Processors
         /// <param name="id"></param>
         /// <returns></returns>
         /// <exception cref="Exception"></exception>
-        public async Task<RocketModel> Get(string id)
+        public async Task<String> Get(string id)
         {
             try
             {
-               RocketModel rocket =  await _rocketService.Get(id);
+                RocketModel rocket = await _rocketService.Get(id);
 
-                //Aici vei implementa partea de parsare smartass 
-                //daca ai intrebari sau nu te descurci
-                //cauti pe google
-                //pana rezolvi, ca eu nu te pot ajuta ca sunt proasta ¯\_(๑❛ᴗ❛๑)_/¯
+                var parser = new JsonLDConvertor(); // instantiaza parser-ul
 
-                return rocket;
+                return parser.MakeStringOutput(rocket);
             }
             catch (Exception ex)
             {
@@ -90,15 +87,15 @@ namespace API.Processors
         /// </summary>
         /// <returns></returns>
         /// <exception cref="Exception"></exception>
-        public async Task<List<RocketModel>> GetAll()
+        public async Task<String> GetAll()
         {
             try
             {
                 List<RocketModel> rockets = await _rocketService.GetAll();
 
-                //si aici
+                var parser = new JsonLDConvertor(); // instantiaza parser-ul
 
-                return rockets;
+                return parser.MakeStringOutput(rockets);
             }
             catch (Exception ex)
             {

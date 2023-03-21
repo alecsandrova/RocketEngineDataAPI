d319f324d6c29653de5d682a3a99a7cdc9352c04
alecsandrova <elle.neagu16@gmail.com>
2023-03-21 23:26:43 +0200

    changed model because of compatibility issues

--- API/Models/RocketModel.cs
+++ API/Models/RocketModel.cs
@@ -9,11 +9,33 @@ namespace API.Models
     {
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
-        public string Id { get; set; }      
+        public string Id { get; set; }
         public string UnitNumber { get; set; }
         public string Time { get; set; }
-        public List<string> OperationalSettings { get; set; }
-        public List<string> SensorMeasurements { get; set; }
+        public string OperationalSetting1 { get; set; }
+        public string OperationalSetting2 { get; set; }
+        public string OperationalSetting3 { get; set; }
+        public string SensorMeasurement1 { get; set; }
+        public string SensorMeasurement2 { get; set; }
+        public string SensorMeasurement3 { get; set; }
+        public string SensorMeasurement4 { get; set; }
+        public string SensorMeasurement5 { get; set; }
+        public string SensorMeasurement6 { get; set; }
+        public string SensorMeasurement7 { get; set; }
+        public string SensorMeasurement8 { get; set; }
+        public string SensorMeasurement9 { get; set; }
+        public string SensorMeasurement10 { get; set; }
+        public string SensorMeasurement11 { get; set; }
+        public string SensorMeasurement12 { get; set; }
+        public string SensorMeasurement13 { get; set; }
+        public string SensorMeasurement14 { get; set; }
+        public string SensorMeasurement15 { get; set; }
+        public string SensorMeasurement16 { get; set; }
+        public string SensorMeasurement17 { get; set; }
+        public string SensorMeasurement18 { get; set; }
+        public string SensorMeasurement19 { get; set; }
+        public string SensorMeasurement20 { get; set; }
+        public string SensorMeasurement21 { get; set; }
 
     }
 }
